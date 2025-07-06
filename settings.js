6e25aa930afe49c6cc1a27c1178a3970328e42b5
TRAY-MD <saintinealex@gmail.com>
2025-07-06 14:56:24 -0400

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,11 +5,12 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Add_Your_Session_Id", // Add your session id
+
+SESSION_ID: process.env.SESSION_ID || "PFoE0DTJ#VUSMBEbzhh4kGkTdgswkK_u-b6PVv0Fq2ig6uy4WKt0", // Add your session id
 PREFIX: process.env.PREFIX || ".",
 MODE: process.env.MODE || "public",  //public | private | inbox | group
-OWNER_NUMBER: process.env.OWNER_NUMBER || "94779062397",
-DEV: process.env.DEV || "94779062397",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "18293125805",
+DEV: process.env.DEV || "18293125805",
 LANG: process.env.LANG || "EN", // EN | SI
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
